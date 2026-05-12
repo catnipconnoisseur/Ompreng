f416fc46f0f6872087a6bca93852bca935e1bb07
catnipconnoisseur <tiffany.christabel.anggriawan@gmail.com>
2026-05-12 10:20:27 +0700

    feat: Implement random falling dishes with ECS components and iPad landscape support
    - Move DishType enum to FoodTypeComponent for proper component architecture
    - Update FoodEntity to use FoodTypeComponent
    - Implement DishSpawner for spawning random dishes at intervals
    - Implement DishFactory for creating dishes
    - Configure GameScene and GameViewController for iPad landscape (1024x768)
    - Add orientation constraints to Info.plist for landscape-only mode
    - Add SwiftUI preview for GameScene
    - Update physics categories for collision detection

--- Ompreng/Components/FoodTypeComponent.swift
+++ Ompreng/Components/FoodTypeComponent.swift
@@ -1,5 +1,51 @@
-import Foundation
+import UIKit
 
-class FoodTypeComponent {
-    // Food type component implementation
+public enum DishType: String, CaseIterable {
+    case rice
+    case chicken
+    case broccoli
+    case apple
+    case milk
+    
+    var displayName: String {
+        switch self {
+        case .rice:
+            return "Rice"
+        case .chicken:
+            return "Chicken"
+        case .broccoli:
+            return "Broccoli"
+        case .apple:
+            return "Apple"
+        case .milk:
+            return "Milk"
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .rice:
+            return UIColor(red: 1.0, green: 0.98, blue: 0.8, alpha: 1.0) // Light beige
+        case .chicken:
+            return UIColor(red: 1.0, green: 0.65, blue: 0.0, alpha: 1.0) // Orange
+        case .broccoli:
+            return UIColor(red: 0.0, green: 0.6, blue: 0.0, alpha: 1.0) // Green
+        case .apple:
+            return UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0) // Red
+        case .milk:
+            return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) // White
+        }
+    }
+    
+    static func random() -> DishType {
+        return allCases.randomElement() ?? .rice
+    }
+}
+
+public class FoodTypeComponent {
+    let dishType: DishType
+    
+    init(dishType: DishType) {
+        self.dishType = dishType
+    }
 }
--- Ompreng/Entities/FoodEntity.swift
+++ Ompreng/Entities/FoodEntity.swift
@@ -1,5 +1,36 @@
-import Foundation
+import SpriteKit
 
-class FoodEntity {
-    // Food entity implementation
+public class FoodEntity: SKSpriteNode {
+    let foodTypeComponent: FoodTypeComponent
+    
+    var dishType: DishType {
+        return foodTypeComponent.dishType
+    }
+    
+    init(dishType: DishType, position: CGPoint) {
+        self.foodTypeComponent = FoodTypeComponent(dishType: dishType)
+        super.init(texture: nil, color: dishType.color, size: CGSize(width: 40, height: 40))
+        
+        self.position = position
+        self.name = "food_\(dishType.rawValue)"
+        self.zPosition = 1
+        
+        // Add physics for collision detection
+        self.physicsBody = SKPhysicsBody(rectangleOf: self.size)
+        self.physicsBody?.affectedByGravity = true
+        self.physicsBody?.allowsRotation = false
+        self.physicsBody?.categoryBitMask = PhysicsCategory.food
+        self.physicsBody?.contactTestBitMask = PhysicsCategory.player
+        self.physicsBody?.collisionBitMask = PhysicsCategory.none
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+public struct PhysicsCategory {
+    static let none: UInt32 = 0
+    static let player: UInt32 = 0x1 << 1
+    static let food: UInt32 = 0x1 << 2
 }
